10b4c115aa6a0e09b53744bc0aef5fc497e298f1
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-26 15:23:16 +0100

    Implemented dto for add product feature

---
+++ Data/Dtos/Product/ProductAddResponseDto.cs
@@ -0,0 +1,12 @@
+namespace Ecommerce_Api.Data.Dtos.Product
+{
+    public class ProductAddResponseDto
+    {
+        public string Message { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
