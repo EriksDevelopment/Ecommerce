e3b72f5c2634f54001af40cdaa69d7d93daaf886
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-28 16:18:18 +0100

    Implemented add to shoppingcart response dto

---
+++ Data/Dtos/ShoppingCart/AddToShoppingCartResponseDto.cs
@@ -0,0 +1,11 @@
+namespace Ecommerce_Api.Data.Dtos.shoppingCart
+{
+    public class AddToShoppingCartResponseDto
+    {
+        public string Message { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
